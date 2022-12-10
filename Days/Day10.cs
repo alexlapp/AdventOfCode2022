bb4748d1b0d4cfa52fc371322b69e9052476e2fe
alexlapp <watertower5132@gmail.com>
2022-12-09 23:41:17 -0600

    Add Day10

---
+++ Days/Day10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code.Days
+{
+    public class Day10 : IPuzzleSolution
+    {
+        public void Run(string input)
+        {
+            int register = 1;
+            int cycle = 1;
+
+            int signalStrengthSum = 0;
+            int[] signalSamples = { 20, 60, 100, 140, 180, 220 };
+
+            foreach (var line in input.Split("\r\n"))
+            {
+                bool increment = false;
+                foreach(var word in line.Split(" "))
+                {
+                    // CRT Draw Code
+                    if (Math.Abs(((cycle-1) % 40) - register) <= 1) { Console.Write("#"); }
+                    else { Console.Write("."); }
+
+                    if (signalSamples.Contains(cycle)) { signalStrengthSum += cycle * register; }
+                    cycle++;
+                    if (increment) { register += int.Parse(word); }
+                    increment = true;
+
+                    if ((cycle -1 ) % 40 == 0) { Console.WriteLine(); } // Move CRT to next line
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"The sum of signal strengths is: {signalStrengthSum}");
+        }
+    }
+}
