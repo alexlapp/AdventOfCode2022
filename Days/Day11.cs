27ab620e6dbdf64222f718c0f65336107e9ee0c5
alexlapp <watertower5132@gmail.com>
2022-12-11 11:48:49 -0600

    Add lcm trick

--- Days/Day11.cs
+++ Days/Day11.cs
@@ -10,16 +10,16 @@ namespace Advent_Of_Code.Days
     class Operation
     {
         public char _operator;
-        private BigInteger? _rightOperand;
+        private long? _rightOperand;
 
         public Operation(string operationString)
         {
             var operations = operationString.Trim().Split(" ").TakeLast(2).ToList();
             _operator = operations[0][0];
-            _rightOperand = BigInteger.TryParse(operations[1], out BigInteger rightOperand) ? rightOperand : null;
+            _rightOperand = long.TryParse(operations[1], out long rightOperand) ? rightOperand : null;
         }
 
-        public BigInteger Operate(BigInteger old)
+        public long Operate(long old)
         {
             switch (_operator)
             {
@@ -35,16 +35,18 @@ namespace Advent_Of_Code.Days
 
     class Monkey
     {
-        private List<BigInteger> _items;
+        private List<long> _items;
         private Operation _operation;
-        private BigInteger _testNumber;
+        private long _testNumber;
         private int _trueTarget;
         private int _falseTarget;
 
-        public BigInteger InspectionCount { get; set; }
-        public int TargetMonkey => (_items.First() % _testNumber == 0) ? _trueTarget : _falseTarget;
+        public long InspectionCount { get; set; }
+        public int TargetMonkey => (_items.First() % TestNumber == 0) ? _trueTarget : _falseTarget;
 
-        public Monkey(List<BigInteger> items, Operation operation, int trueTarget, int falseTarget, BigInteger testNumber)
+        public long TestNumber { get => _testNumber; }
+
+        public Monkey(List<long> items, Operation operation, int trueTarget, int falseTarget, long testNumber)
         {
             _items = items;
             _operation = operation;
@@ -57,27 +59,27 @@ namespace Advent_Of_Code.Days
 
         public static Monkey Parse(string block)
         {
-            List<BigInteger> monkeyItems = new List<BigInteger>();
+            List<long> monkeyItems = new List<long>();
 
             var info = block.Split("\r\n");
             foreach(var word in info[1].Replace(",", "").Split(" "))
             {
-                if (BigInteger.TryParse(word, out var item)) { monkeyItems.Add(item); }
+                if (long.TryParse(word, out var item)) { monkeyItems.Add(item); }
             }
 
             var operation = new Operation(info[2]);
             
-            var test = BigInteger.Parse(info[3].Split(" ").Last());
+            var test = long.Parse(info[3].Split(" ").Last());
             var trueTarget = int.Parse(info[4].Split(" ").Last());
             var falseTarget = int.Parse(info[5].Split(" ").Last());
 
             return new Monkey(monkeyItems, operation, trueTarget, falseTarget, test);
         }
 
-        public void InspectItem()
+        public void InspectItem(long lcm)
         {
             InspectionCount++;
-            _items[0] = _operation.Operate(_items[0]);// / 3;
+            _items[0] = _operation.Operate(_items[0]) % lcm;// / 3;
         }
 
         public bool HasItem()
@@ -85,12 +87,12 @@ namespace Advent_Of_Code.Days
             return _items.Any();
         }
 
-        public void ReceiveItem(BigInteger item)
+        public void ReceiveItem(long item)
         {
             _items.Add(item);
         }
 
-        public BigInteger ThrowItem()
+        public long ThrowItem()
         {
             var result = _items[0];
             _items.RemoveAt(0);
@@ -110,14 +112,15 @@ namespace Advent_Of_Code.Days
                 monkeys.Add(Monkey.Parse(m));
             }
 
+            long lcm = monkeys.Aggregate((long)1, (acc, m) => acc * m.TestNumber);
+
             for (int i = 0; i < 10000; i++)
             {
-                Console.WriteLine(i);
                 foreach (var monkey in monkeys)
                 {
                     while (monkey.HasItem())
                     {
-                        monkey.InspectItem();
+                        monkey.InspectItem(lcm);
                         monkeys[monkey.TargetMonkey].ReceiveItem(monkey.ThrowItem());
                     }
                 }
@@ -127,9 +130,7 @@ namespace Advent_Of_Code.Days
                 .Select(m => m.InspectionCount)
                 .OrderByDescending(ic => ic)
                 .Take(2)
-                .Aggregate((BigInteger)1, (acc, ic) => acc * ic);
-
-            Console.WriteLine(monkeys.Select(x => x.InspectionCount).OrderByDescending(x => x).Select(x => x.ToString()).Aggregate("", (acc, t) => acc += ", " + t));
+                .Aggregate((long)1, (acc, ic) => acc * ic);
 
             Console.WriteLine($"Monkey Business value: {monkeyBusiness}");
         }
