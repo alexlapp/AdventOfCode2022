3d84273a7753f894812cd494e6d122c3dfda87fe
alexlapp <watertower5132@gmail.com>
2022-12-12 00:12:06 -0600

    Day 12 Part 1

---
+++ Days/Day12.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code.Days
+{
+    class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool Equals(Coord b)
+        {
+            return X == b.X && Y == b.Y;
+        }
+    }
+
+    class Map
+    {
+        public char[] Squares { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Map(char[] squares, int width, int height)
+        {
+            Squares = squares;
+            Width = width;
+            Height = height;
+        }
+
+        public char GetSquare(int index)
+        {
+            return Squares[index];
+        }
+
+        public char GetSquare(Coord coord)
+        {
+            return Squares[CoordToIndex(coord)];
+        }
+
+        public Coord IndexToCoord(int index)
+        {
+            return new Coord { X = index % Width, Y = index / Width};
+        }
+
+        public int CoordToIndex(Coord coord)
+        {
+            return (coord.Y * Width) + coord.X;
+        }
+
+        public Coord? Above(Coord start)
+        {
+            if (start.Y == 0) return null;
+
+            return new Coord { X = start.X, Y = start.Y - 1 };
+        }
+
+        public Coord? Below(Coord start)
+        {
+            if (start.Y == Height - 1) return null;
+
+            return new Coord { X = start.X, Y = start.Y + 1 };
+        }
+
+        public Coord? Left(Coord start)
+        {
+            if (start.X == 0) return null;
+
+            return new Coord { X = start.X - 1, Y = start.Y };
+        }
+
+        public Coord? Right(Coord start)
+        {
+            if (start.X == Width - 1) return null;
+
+            return new Coord { X = start.X + 1, Y = start.Y };
+        }
+    }
+
+    class VisitCoord
+    {
+        public Coord Coord { get; set; }
+        public int Distance { get; set; }
+
+        public VisitCoord(Coord coord, int distance)
+        {
+            Coord = coord;
+            Distance = distance;
+        }
+    }
+
+    public class Day12 : IPuzzleSolution
+    {
+        public void Run(string input)
+        {
+            var width = input.IndexOf('\r');
+            var height = input.Count(c => c.Equals('\n')) + 1;
+            var rawSquares = input.Replace("\r\n", "");
+            var squares = rawSquares.ToCharArray();
+            var map = new Map(squares, width, height);
+
+            var startingCoord = map.IndexToCoord(rawSquares.IndexOf('S'));
+            var targetCoord = map.IndexToCoord(rawSquares.IndexOf('E'));
+
+            var visitedNodes = new HashSet<int>();
+            var nodesToVisit = new Queue<VisitCoord>();
+            nodesToVisit.Enqueue(new VisitCoord(startingCoord, 0));
+
+            var distance = 0;
+            while (nodesToVisit.Any())
+            {
+                var node = nodesToVisit.Dequeue();
+                
+                if (visitedNodes.Contains(map.CoordToIndex(node.Coord))) { continue; }
+                visitedNodes.Add(map.CoordToIndex(node.Coord));
+
+                if (node.Coord.Equals(targetCoord)) 
+                {
+                    distance = node.Distance;
+                    break;
+                }
+
+                var above = map.Above(node.Coord);
+                var below = map.Below(node.Coord);
+                var left = map.Left(node.Coord);
+                var right = map.Right(node.Coord);
+
+                if (above != null && CanTraverse(map.GetSquare(node.Coord), map.GetSquare(above))) 
+                    { nodesToVisit.Enqueue(new VisitCoord(above, node.Distance + 1)); }
+
+                if (below != null && CanTraverse(map.GetSquare(node.Coord), map.GetSquare(below)))
+                    { nodesToVisit.Enqueue(new VisitCoord(below, node.Distance + 1)); }
+
+                if (left != null && CanTraverse(map.GetSquare(node.Coord), map.GetSquare(left)))
+                    { nodesToVisit.Enqueue(new VisitCoord(left, node.Distance + 1)); }
+
+                if (right != null && CanTraverse(map.GetSquare(node.Coord), map.GetSquare(right)))
+                    { nodesToVisit.Enqueue(new VisitCoord(right, node.Distance + 1)); }
+            }
+
+            Console.WriteLine($"No way this works: {distance}");
+        }
+
+        private bool CanTraverse(char start, char end)
+        {
+            var realStart = start.Equals('S') ? 'a' : start;
+            var realEnd = end.Equals('E') ? 'z' : end;
+            return (int)realEnd - (int)realStart <= 1;
+        }
+    }
+}
