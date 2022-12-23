01e5c6723413e227ac42610532ade322744662a1
alexlapp <watertower5132@gmail.com>
2022-12-23 07:39:16 -0600

    Add missing days

---
+++ Days/Day23.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code.Days
+{
+    public class Day23 : IPuzzleSolution
+    {
+        public void Run(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
